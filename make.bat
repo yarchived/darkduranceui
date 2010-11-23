b851a92846f144c3f98541b1d2fdf383f61a9aba
yaroot <yaroot@gmail.com>
2010-11-23 14:38:33 +0800

    cmd.exe sucks

---
+++ make.bat
@@ -0,0 +1,10 @@
+
+
+cd ..
+mklink /d DarkDurance_BuffFrame    darkduranceui-git\DarkDurance_BuffFrame
+mklink /d DarkDurance_Minimap      darkduranceui-git\DarkDurance_Minimap
+mklink /d DarkDurance_UnitFrame    darkduranceui-git\DarkDurance_UnitFrame
+mklink /d DarkDurance_ActionBar    darkduranceui-git\DarkDurance_ActionBar
+
+pause
+
