7744c033737a7f0216623894323690c22b7f59c7
yaroot <yaroot@gmail.com>
2011-09-29 17:52:00 +0800

    mklink

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 .PHONY: clean pack
 
 pack: clean
-	tar jcvf DarkDuranceUI.tar.bz2 DarkDurance_
+	tar jcvf DarkDuranceUI.tar.bz2 DarkDurance_*
 
 clean:
 	rm -f *.tar.bz2
---
+++ mklink.bat
@@ -0,0 +1,10 @@
+
+
+cd ..
+mklink /d DarkDurance_BuffFrame    DarkDuranceUI\DarkDurance_BuffFrame
+mklink /d DarkDurance_Minimap      DarkDuranceUI\DarkDurance_Minimap
+mklink /d DarkDurance_UnitFrame    DarkDuranceUI\DarkDurance_UnitFrame
+mklink /d DarkDurance_ActionBar    DarkDuranceUI\DarkDurance_ActionBar
+
+pause
+
