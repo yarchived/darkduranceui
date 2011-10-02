2277ad463f095c4d34f7aaf32c179a725e5a13cd
yaroot <yaroot@gmail.com>
2011-10-02 15:37:19 +0800

    layout factory

---
+++ DarkDurance_UnitFrame/core/layout.lua
@@ -0,0 +1,50 @@
+
+local _NAME, _NS = ...
+local oUF = _NS.oUF
+local DDUF = _NS[_NAME]
+
+DDUF.styles = {}
+
+local style_mt = {
+    __call = function(funcs, self, ...)
+        for _, func in next, funcs do
+            func(self, ...)
+        end
+    end,
+}
+
+function DDUF:UnitStyle(unit, func)
+    if(type(units) == 'table') then
+        for _, u in next, unit do
+            self:UnitStyle(u, func)
+        end
+    else
+        if(not self.styles[unit]) then
+            self.styles = setmetatable({}, style_mt)
+        end
+        tinsert(self.styles[unit], func)
+    end
+end
+
+function DDUF:UnitCommonStyle(func)
+    self:UnitStyle('common', func)
+end
+
+function DDUF:Spawn(func)
+    oUF:Factory(func)
+end
+
+oUF:RegisterStyle(_NAME, function(self, unit)
+    local funcs = DDUF.styles[unit]
+    if(funcs) then
+        DDUF.styles.common(self, unit)
+        funcs(self, unit)
+    else
+
+    end
+end)
+
+oUF:SetActiveStyle(_NAME)
+
+DDUF:UnitCommonStyle(function() end)
+
