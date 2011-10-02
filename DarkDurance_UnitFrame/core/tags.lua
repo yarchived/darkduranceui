f8caac9a277010e161a32f17c59c8c93364170d5
yaroot <yaroot@gmail.com>
2011-10-02 23:58:20 +0800

    Move `CreateTag` to meta api

--- DarkDurance_UnitFrame/core/tags.lua
+++ DarkDurance_UnitFrame/core/tags.lua
@@ -2,11 +2,14 @@
 local _NAME, _NS = ...
 local DDUF, oUF = _NS[_NAME], _NS.oUF
 
-function DDUF:CreateTag(obj, region, tagstr, call)
+-- better tag api
+function CreateTag(self, region, tagstr, call)
     local fs = region:CreateFontString()
     call(fs)
-    obj:Tag(fs, tagstr)
+    self:Tag(fs, tagstr)
 
     return fs
 end
+
+oUF:RegisterMetaFunction('CreateTag', CreateTag)
 
