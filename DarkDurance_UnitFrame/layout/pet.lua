187392e03559106c5d3dea440c41646dfd2f39d2
yaroot <yaroot@gmail.com>
2011-10-06 14:15:34 +0800

    blah

--- DarkDurance_UnitFrame/layout/pet.lua
+++ DarkDurance_UnitFrame/layout/pet.lua
@@ -3,11 +3,13 @@ local _NAME, _NS = ...
 local DDUF, oUF = _NS[_NAME], _NS.oUF
 local media = DDUF.media
 
-DDUF:UnitStyle('pet', function(self, unit)
-    self:SetSize(120, 33)
+local _UNIT = 'pet'
+
+DDUF:UnitStyle(_UNIT, function(self, unit)
+    self:SetSize(100, 33)
 end)
 
-DDUF:UnitStyle('pet', function(self, unit)
+DDUF:UnitStyle(_UNIT, function(self, unit)
     local fore = self.FG:CreateTexture(nil, 'ARTWORK')
     fore:SetAllPoints(self.FG)
 
@@ -22,19 +24,46 @@ DDUF:UnitStyle('pet', function(self, unit)
     self.Textures[fore] = file
 end)
 
-DDUF:UnitStyle('pet', function(self, unit)
-    local bg = self.BG:CreateTexture(nil, 'BORDER')
+DDUF:UnitStyle(_UNIT, function(self, unit)
+    local bg = self.BG:CreateTexture(nil, 'BACKGROUND')
     bg:SetAllPoints(self.FG)
 
-    local file = media.tot.bg
-    bg:SetTexture(file)
+    local file = media.tot.tot
+    bg:SetTexture(media.getTexture(file))
+
     self.BG.Texture = bg
     self.Textures[bg] = file
 end)
 
+DDUF:UnitStyle(_UNIT, function(self, unit)
+    local hp = CreateFrame('StatusBar', nil, self.BG)
+    self.Health = hp
+
+    hp:SetStatusBarTexture(media.roth)
+    hp:SetSize(55, 12)
+    hp:SetPoint('TOPLEFT', self, 'CENTER', -12, 5)
+
+    hp.colorClass = true
+
+    hp.bg = hp:CreateTexture(nil, 'BORDER')
+    hp.bg:SetTexture(media.roth)
+    hp.bg:SetAllPoints()
+    hp.bg.multiplier = .3
+end)
+
+DDUF:UnitStyle(_UNIT, function(self, unit)
+    local portrait = CreateFrame('PlayerModel', nil, self.BG)
+    self.Portrait = portrait
+
+    portrait:SetPoint('TOPRIGHT', self, 'CENTER', -20, 15)
+
+    local _SIZE = 30
+    portrait:SetSize(_SIZE, _SIZE)
+end)
+
 DDUF:Spawn(function()
-    local pet = oUF:Spawn'pet'
-    pet:SetPoint('CENTER', DDUF.units.player, 0, -30)
-    DDUF.units.pet = pet
+    local f = oUF:Spawn(_UNIT)
+    f:SetPoint('TOPLEFT', DDUF.units.player, 'BOTTOMLEFT', 0, -10)
+    DDUF.units[_UNIT] = f
 end)
 
