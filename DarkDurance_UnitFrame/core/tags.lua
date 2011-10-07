3ba038da0e4b65914fbbffda14d89e6555f000bf
yaroot <yaroot@gmail.com>
2011-10-07 15:41:54 +0800

    Add tags and hook _ENV

--- DarkDurance_UnitFrame/core/tags.lua
+++ DarkDurance_UnitFrame/core/tags.lua
@@ -24,3 +24,53 @@ function(u)
 end
 ]]
 
+oUF.Tags['dd:smarthp'] = [[
+    function(u, r)
+        local cur, max = UnitHealth(u), UnitHealthMax(u)
+        if(not max) then return end
+        if(max == 0) then max = 1 end
+
+        if(not UnitIsConnected(r or u)) then
+            return 'Offline'
+        elseif(UnitIsGhost(u)) then
+            return 'Ghost'
+        elseif(UnitIsFeignDeath(u)) then
+            return '|cffff3333FD|r'
+        elseif(UnitIsDead(u)) then
+            return 'Dead'
+        else
+            if(cur == max) then
+                return '|cff98c290' .. Truncate(max)
+            else
+                local r, g, b = oUF.ColorGradient(cur, max, 245/255, 68/255, 68/255, 245/255, 186/255, 69/255, 105/255, 201/255, 105/255)
+                local color = Hex(r, g, b)
+                return ('|cfffd5c69%s |cffdbf6db- %s%d%%|r'):format(Truncate(cur), color, floor(cur/max*100))
+            end
+        end
+    end
+]]
+oUF.TagEvents['dd:smarthp'] = oUF.TagEvents['missinghp']
+
+oUF.Tags['dd:realname'] = [[
+    function(u, r)
+        return UnitName(r or u)
+    end
+]]
+
+-- hook into `_ENV' in tags.lua
+-- but `_ENV' is a metatable, we need to call `rawget' and `rawset'
+-- to manipulate it.
+local _ENV
+do
+    local dummy_name = 'Zei1aeLuiedoo7EeNoop0veeOhneij3aOi0shuLeAipeiPh3ohZ5aa9fZequ5guz'
+    oUF.Tags[dummy_name] = [[ function() return end ]]
+
+    local func = oUF.Tags[dummy_name]
+    _ENV = getfenv(func)
+
+    -- remove the func
+    rawset(oUF.Tags, dummy_name, nil)
+end
+
+rawset(_ENV, 'Truncate', DDUF.TruncateNumber)
+
--- DarkDurance_UnitFrame/core/utils.lua
+++ DarkDurance_UnitFrame/core/utils.lua
@@ -22,3 +22,12 @@ function DDUF:TestBackdrop(obj)
     obj:SetBackdropColor(1, 0, 0, .5)
 end
 
+function DDUF.TruncateNumber(value)
+    if(value >= 1e6) then
+        value = format('%.1fm', value / 1e6)
+    elseif(value >= 1e3) then
+        value = format('%.1fk', value / 1e3)
+    end
+    return gsub(value, '%.?0+([km])$', '%1')
+end
+
